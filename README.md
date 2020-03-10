9904706180195b65f119585df54406a5235de6dd
Diana-Flores <vilma_diana@outlook.com>
2020-03-10 19:01:49 -0300

    Subo Fuentes y Cabecera

---
+++ README.md
@@ -0,0 +1,1 @@
+# Juanito-Travel
---
+++ js/script.js
@@ -0,0 +1,15 @@
+/*=============================================
+BANNER
+=============================================*/
+
+$(".fade-slider").jdSlider({
+
+	isSliding: false,
+	isAuto: true,
+	isLoop: true,
+	isDrag: false,
+	interval:5000,
+	isCursor: false,
+	speed:3000
+
+});
